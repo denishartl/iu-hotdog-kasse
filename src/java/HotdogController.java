09a0515df5a4199f395e003acaef681b63e766a9
denishartl <denis.hartl@icloud.com>
2022-03-19 13:42:21 +0100

    added discount system

--- src/java/HotdogController.java
+++ src/java/HotdogController.java
@@ -23,11 +23,30 @@ public class HotdogController {
     public static void updatePreis() {
         double preis = 0.00;
         ArrayList<Zutat> zutaten = Konfigurator.getInstance().getHotdog().getZutaten();
+        int anzahl_zutaten = 0;
+        double rabatt_prozent = 0.0;
         
         for (Zutat zutat:zutaten) {
             if (zutat.getAusgewaehlt()) {
+                anzahl_zutaten = anzahl_zutaten + 1;
+                 if (anzahl_zutaten == 2) {
+                    rabatt_prozent = 10.0;
+                }
+                else if (anzahl_zutaten == 3) {
+                    rabatt_prozent = 20.0;
+                }
+                else if (anzahl_zutaten == 4) {
+                    rabatt_prozent = 30.0;
+                }
+                else if (anzahl_zutaten == 5) {
+                    rabatt_prozent = 40.0;
+                }
+                else if (anzahl_zutaten > 5) {
+                    rabatt_prozent = 50.0;
+                }
                 System.out.println(zutat.getName());
-                preis = preis + zutat.getPreis();
+                System.out.println(rabatt_prozent/100);
+                preis = preis + (zutat.getPreis()*(1-(rabatt_prozent/100)));
             }
         }
         System.out.println(preis);
